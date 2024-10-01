965796eb44f5180f4a380143eeada3a92d021060
Adis Kljajic <adis.kljajic@outlook.com>
2024-10-01 14:41:18 -0700

    Add Additional Folders To Git

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		unoptimized: true
+	},
+	// Enable static export
+	output: 'export',
+};
 
 export default nextConfig;
